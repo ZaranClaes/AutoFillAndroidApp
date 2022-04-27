6bff8e4a70dc3fd945368d3654f345e52b23868f
ZaranClaes <103043122+ZaranClaes@users.noreply.github.com>
2022-04-27 22:04:01 +0800

    Update GoOnlineThree.java

--- JavaCode/GoOnlineThree.java
+++ JavaCode/GoOnlineThree.java
@@ -2,12 +2,16 @@ package com.example.autofillversion1official;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.lang.reflect.Array;
@@ -23,6 +27,7 @@ public class GoOnlineThree extends AppCompatActivity {
     String url = "https://zh.surveymonkey.com/r/EmployeeHealthCheck";
     String doneUrl = "https://zh.surveymonkey.com/r/HCCompleted";
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,11 @@ public class GoOnlineThree extends AppCompatActivity {
         temperature = "36";
         employeeID = sharedPreferences.getString("ID3", "");
 
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        // Initializing the calendar Object for today's date
+        Calendar cal = Calendar.getInstance();
+        //using sdf to format to mm/dd/yyyy
+        String curr_date = sdf.format(cal.getTime());
 
         webView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
@@ -46,15 +56,16 @@ public class GoOnlineThree extends AppCompatActivity {
             public void onPageFinished(WebView webView, String url) {
                 //webView.loadUrl("javascript:document.getElementById('763128014').value = '" + employeeID1 + "';");
 
-                webView.loadUrl("javascript:(function() { document.getElementById('62391608_516129376').click(); " +
-                        "document.getElementById('62391605').value = '" + employeeID + "'; " +
-                        "document.getElementById('62391610_516129380').click(); " +
-                        "document.getElementById('62391606').value = '" + temperature + "';" +
-                        "document.getElementById('62391616_516129418').click();" +
-                        "document.getElementById('62391993_516132409').click();" +
-                        "document.getElementById('62391615_516129447').click();" +
-                        "document.getElementById('62391617_516129442').click();" +
-                        "document.getElementById('62391607_516129368').click();" +
+                webView.loadUrl("javascript:(function() { document.getElementById('66405067_542650090').click(); " +
+                        "document.getElementById('66405064').value = '" + employeeID + "'; " +
+                        "document.getElementById('66405069_542650094').click(); " +
+                        "document.getElementById('66405065').value = '" + temperature + "';" +
+                        "document.getElementById('66405075_542650132').click();" +
+                        "document.getElementById('66405078_542650162').click();" +
+                        "document.getElementById('66405129_542650744_DMY').value = '" + curr_date + "';" +
+                        "document.getElementById('66405074_542650161').click();" +
+                        "document.getElementById('66405076_542650156').click();" +
+                        "document.getElementById('66405066_542650082').click();" +
                         "document.getElementsByClassName('btn small next-button survey-page-button user-generated notranslate')[0].click(); ;})()");
                 webUrl = webView.getUrl();
 
@@ -72,3 +83,4 @@ public class GoOnlineThree extends AppCompatActivity {
     }
 
 }
+
